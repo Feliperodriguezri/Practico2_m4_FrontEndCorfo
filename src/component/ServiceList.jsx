87e7f6629608090ec752c7bd2bce4a7a045e93fa
felipe rodriguez <felipe.rodriguezri@gmail.com>
2024-12-23 21:49:42 -0300

    Se trabajan en la preguntas 4 y 5, se dejaron listas, pendiente solo punto 6

--- src/component/ServiceList.jsx
+++ src/component/ServiceList.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { HospitalContext } from '../App';
 
 function ServiceList(props) {
-    const { servicios } = useContext(HospitalContext)
-    const servicio = servicios.find(ser => ser === props.servicio)
+    const { servicios } = useContext(HospitalContext);
+    const servicio = servicios && servicios.find(ser => ser === props.servicio);
 
     if (!servicio) {
-        return <div>Servicio not found</div>
+        return <div>Servicio no encontrado</div>;
     }
     return (
         <li className="list-group-item">{servicio}</li>
-    )
+    );
 }
+
+ServiceList.propTypes = {
+    servicio: PropTypes.string.isRequired,
+};
 
 export default ServiceList;
